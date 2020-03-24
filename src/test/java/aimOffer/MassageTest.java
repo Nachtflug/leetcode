7b5da0df5c575587f6d152bfc48d2f94a3e7899e
Nachtflug <363531619@qq.com>
2020-03-24 13:52:06 +0800

    daily

---
+++ src/test/java/aimOffer/MassageTest.java
@@ -0,0 +1,15 @@
+package aimOffer;
+
+public class MassageTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(Massage.massage(new int[]{2,1,4,5,3,1,1,3}));
+        System.out.println(Massage.massage(new int[]{2,7,9,3,1}));
+        System.out.println(Massage.massage(new int[]{1,2,3,1}));
+        System.out.println(Massage.massage(new int[]{}));
+        System.out.println(Massage.massage(new int[]{1}));
+        System.out.println(Massage.massage(new int[]{1, 2}));
+    }
+
+}
