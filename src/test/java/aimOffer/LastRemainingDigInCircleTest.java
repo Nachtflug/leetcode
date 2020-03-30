4034d9331f70c992fe68c0037b2f5d27aca6c587
Nachtflug <363531619@qq.com>
2020-03-30 14:06:01 +0800

    daily

---
+++ src/test/java/aimOffer/LastRemainingDigInCircleTest.java
@@ -0,0 +1,13 @@
+package aimOffer;
+
+public class LastRemainingDigInCircleTest {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(LastRemainingDigInCircle.lastRemaining(3, 5));
+        System.out.println(LastRemainingDigInCircle.lastRemaining(10, 17));
+
+    }
+
+}
